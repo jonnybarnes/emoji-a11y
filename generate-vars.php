5f3b340109c8e6a2d76e3fedc270ac9ea41e4cec
Jonny Barnes <jonny@jonnybarnes.uk>
2017-02-01 14:40:08 +0000

    Better comments/output in the genreate-vars.php script

--- generate-vars.php
+++ generate-vars.php
@@ -1,10 +1,36 @@
 <?php
 
-// this file generates the emoji array and regular expression from the full emoji list:
-// http://unicode.org/emoji/charts/full-emoji-list.html
+/*
+ * The file `src/EmojiModifier.php` contains two variables defined in the
+ * contructor. An array where the keys are the unicode codepoints of the various
+ * emoji sequences, and the values are the associated descriptions. There is
+ * also a string which is a regular expression to match the emoji sequences.
+ * These variables are derived from the unicode conosrtium maintained full emoji
+ * list. This can be found at:
+ * <http://unicode.org/emoji/charts/full-emoji-list.html>.
+ */
+
+try {
+    $fullEmojiList = new SplFileObject('./full-emoji-list.html');
+} catch (RuntimeException $exception) {
+    //file doesn’t exist, so lets create it.
+    echo 'Getting source file.' . PHP_EOL;
+    $fullEmojiList = new SplFileObject('./full-emoji-list.html', 'w');
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => 'http://unicode.org/emoji/charts/full-emoji-list.html',
+        CURLOPT_HEADER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+    ]);
+    $html = curl_exec($ch);
+    $fullEmojiList->fwrite($html);
+}
 
 $emoji = [];
 
+echo 'Reading/parsing the html file.' . PHP_EOL;
+libxml_use_internal_errors(true);
 $dom = new DOMDocument();
 $dom->preserveWhiteSpace = false;
 $dom->loadHTMLFile('./full-emoji-list.html');
